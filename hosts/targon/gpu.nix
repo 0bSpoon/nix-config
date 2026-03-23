3333405f7bef1af46356c971eff51f62227cabf1
0bSpoon <bSpoon@outlook.jp>
2026-03-23 19:25:27 +0900

    update: NVIDIA driver  を無効化

--- hosts/targon/gpu.nix
+++ hosts/targon/gpu.nix
@@ -8,4 +8,7 @@
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver  # VA-API (第8世代以降)
   ];
+
+  # NVIDIA GPU (dGPU) を無効化
+  boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_drm" "nvidia_modeset" "nouveau" ];
 }
