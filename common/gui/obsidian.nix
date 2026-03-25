399fbfb25506e1e8fbd41b282de6fbb1ae8ffd55
0bSpoon <bSpoon@outlook.jp>
2026-03-26 05:01:25 +0900

    update: Obsidianボルトのディレクトリ名をvaultに変更

--- common/gui/obsidian.nix
+++ common/gui/obsidian.nix
@@ -4,9 +4,9 @@
   home.packages = [ pkgs.obsidian ];
 
   home.activation.cloneObsidianVault = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [ ! -d "$HOME/Documents/main/.git" ]; then
+    if [ ! -d "$HOME/Documents/vault/.git" ]; then
       export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh"
-      run ${pkgs.git}/bin/git clone git@github.com:0bSpoon/obsidian-vault.git "$HOME/Documents/main"
+      run ${pkgs.git}/bin/git clone git@github.com:0bSpoon/obsidian-vault.git "$HOME/Documents/vault"
     fi
   '';
 }
