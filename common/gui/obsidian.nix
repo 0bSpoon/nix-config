20f9e2373e2d092348dadc02d2f7d54c33946533
0bSpoon <bSpoon@outlook.jp>
2026-04-04 13:52:32 +0900

    Manage Obsidian vault clone with user service

--- common/gui/obsidian.nix
+++ common/gui/obsidian.nix
@@ -3,10 +3,31 @@
 {
   home.packages = [ pkgs.obsidian ];
 
-  # home.activation.cloneObsidianVault = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-  #   if [ ! -d "$HOME/Documents/vault/.git" ]; then
-  #     export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh"
-  #     run ${pkgs.git}/bin/git clone git@github.com:0bSpoon/obsidian-vault.git "$HOME/Documents/vault"
-  #   fi
-  # '';
+  systemd.user.services.clone-obsidian-vault = {
+    Unit = {
+      Description = "Clone Obsidian vault on first login";
+      After = [ "sops-nix.service" ];
+      Requires = [ "sops-nix.service" ];
+      ConditionPathExists = [
+        "%h/.ssh/github_key"
+        "!%h/Documents/vault/.git"
+      ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecStart = toString (
+        pkgs.writeShellScript "clone-obsidian-vault" ''
+          set -eu
+
+          mkdir -p "$HOME/Documents"
+
+          export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh -F /dev/null -i $HOME/.ssh/github_key -o IdentitiesOnly=yes -o StrictHostKeyChecking=accept-new"
+          exec ${pkgs.git}/bin/git clone git@github.com:0bSpoon/obsidian-vault.git "$HOME/Documents/vault"
+        ''
+      );
+    };
+
+    Install.WantedBy = [ "default.target" ];
+  };
 }
